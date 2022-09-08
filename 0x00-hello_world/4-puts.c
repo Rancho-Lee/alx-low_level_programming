f8f7e5d6950172ff380377c300755f4f25b4ca54
Rancho-Lee <hairdrap7@gmail.com>
2022-09-08 14:14:11 -0700

    step0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-int main(void)/*main:entry point*/
-	/**
-	 *main:entry point
-	 *
-	 * Description:using put instead of printf
-	 *
-	 * Return:Always 0 (success)
-	*/
+/**
+ *main:entry point
+ *
+ * Description:using put instead of printf
+ *
+ * Return:Always 0 (success)
+*/
+int main(void)
 {
 	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(Mystr); /*writing the string to stdout*/
 	return (0);
 }
