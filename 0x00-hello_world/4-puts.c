a4975b0756d52b57ea0ed2c862232a2f320c08e7
Rancho-Lee <hairdrap7@gmail.com>
2022-09-08 12:27:47 -0700

    step4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,15 +2,14 @@
 
 int main(void)
 	/**
-	 *main - entry point 
+	 *main:entry point
 	 *
-	 * Description: using put instead of printf
+	 * Description:using put instead of printf
 	 *
-	 * Return: Always 0 (success)
+	 * Return:Always 0 (success)
 	*/
 {
 	char Mystr[] = "\"Programming is like building a multilingual puzzle";
 	puts(Mystr);
-
 	return (0);
 }
