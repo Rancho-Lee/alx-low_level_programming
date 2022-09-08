e80e462c6d3474fe780546e9b9f8f4de7dc96068
Rancho-Lee <hairdrap7@gmail.com>
2022-09-08 13:02:17 -0700

    step5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-int main(void) /**
-		*main: taking a void
-		*
-		* Description: using a printf instead of put
-		*
-		* Return: Always 0 (success)
-		*/
+/**
+ * main: taking a void
+ *
+ * Description: using a printf instead of put
+ * 
+ * Return: Always 0 (success)
+ */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
