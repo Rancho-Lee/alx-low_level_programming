e51da5e2f29039505e7d44f45c887523d3b987f7
Rancho-Lee <hairdrap7@gmail.com>
2022-09-09 16:55:38 -0700

    step2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - This is based on putchar
+ *
+ * Return: 0;
+ *
+ */
+int main(void)
+{
+	/*declaring my data type and any variable*/
+	char start;
+	char end;
+	char ch;
+
+	start = 'a';
+	end = 'z';
+	ch = start;
+
+	while (start != end)
+	{
+		putchar(ch) putchar('\n');
+		ch++;
+	}
+	return (0);
+
+
+}
