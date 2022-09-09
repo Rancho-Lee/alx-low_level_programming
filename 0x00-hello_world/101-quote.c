560fb620bdcf837d07db475ec449f2b7daff2db1
Rancho-Lee <hairdrap7@gmail.com>
2022-09-08 17:09:52 -0700

    step6

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints a quote to std out
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	write(2, "and that piece of artis useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
