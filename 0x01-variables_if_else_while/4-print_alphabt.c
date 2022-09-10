1e91e8546e21b5dc23365f202cab429d4822289e
Rancho-Lee <hairdrap7@gmail.com>
2022-09-10 03:39:45 -0700

    step4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Letting some numbers slide
+ *
+ * Return: 0;
+ *
+ */
+int main(void)
+{
+	int start;
+	int end;
+
+	start = 97;
+	end = 122;
+
+	while (start <= end)
+	{
+		if (start !113 || !101)
+		{
+			putchar(start);
+			start++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
