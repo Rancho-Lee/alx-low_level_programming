3704cb52f356bb3c01a191f4dc64d8913617a712
Rancho-Lee <hairdrap7@gmail.com>
2022-09-10 15:29:44 -0700

    step100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,23 +10,19 @@ int main(void)
 	int start;
 	int end;
 	int start2;
-	int end2;
 
 	start = 48;
 	end = 57;
 	start2 = 48;
-	end2 = 57;
 
 	while (start <= end)
 	{
-		while (start2 <= end2)
+		while (start2 <= end)
 		{
 			putchar(start);
 			putchar(start2);
 			start2++;
 		}
-		putchar(start);
-		putchar(start2);
 		start++;
 	}
 	putchar('\n');
