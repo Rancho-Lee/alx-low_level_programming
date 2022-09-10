1723192a4af6df8bdf656ba85d8f7610591f6608
Rancho-Lee <hairdrap7@gmail.com>
2022-09-10 10:27:18 -0700

    step9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Combination of digits
+ *
+ * Return: 0;
+ *
+ */
+int main(void)
+{
+	int num;
+	int com;
+
+	num = 48..57;
+	com = {num}{num}
+
+	putchar(com);
+	return (0);
+}
