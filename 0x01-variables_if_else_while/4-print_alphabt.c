e4d50c9373579c7190018939749da2a0f6694d17
Rancho-Lee <hairdrap7@gmail.com>
2022-09-10 03:12:31 -0700

    step4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Alphabet is neccessary
+ *
+ * Return: 0;
+ *
+ */
+int main(void)
+{
+	int start;
+	int end;
+
+	start = 48;
+	end = 57;
+
+	while (start <= end)
+	{
+		putchar(start);
+		start++;
+	}
+	return (0);
+}
