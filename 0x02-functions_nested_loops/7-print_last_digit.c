8e6ffc5c0b1b432fda215804698b3cd2e16dfee4
Rancho-Lee <hairdrap7@gmail.com>
2022-09-13 18:34:09 -0700

    step7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - last Digit
+ *
+ * @r: parameter
+ *
+ * Return: 0;
+ *
+ */
+int print_last_digit(int r)
+{
+	int n;
+	
+	n = r % 10;
+
+	_print_last_digit(n);
+	return (0);
+}
