7b156ef1e71cba221da90c7a76373599d6168b9e
Rancho-Lee <hairdrap7@gmail.com>
2022-09-13 18:10:49 -0700

    sep5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,21 +9,17 @@
  */
 int print_sign(int n)
 {
-	int r;
-	
-	r = _putchar(n);
-	
-	if (r > 0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (r == 0)
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
-	else if (r < 0)
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
