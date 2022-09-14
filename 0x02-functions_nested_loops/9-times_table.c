a12484a07532677c7ff8d77ddf48a0b68e2f8776
Rancho-Lee <hairdrap7@gmail.com>
2022-09-14 15:11:44 -0700

    step8

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * times_table - getting familiar with the times table
+ *
+ * Return: void;
+ *
+ */
+void times_table(void)
+{
+	int x, y, z;
+
+	for (x = 0; x < 10; x++)
+	{
+		y = x * z;
+		for (z = 0; z < 10; z++)
+		{
+			_putchar((y % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
