12ac45da73632486452c33255e08bc7e9ea37fdb
Rancho-Lee <hairdrap7@gmail.com>
2022-09-14 16:14:06 -0700

    step10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - addition 
+ *
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: addition result
+ *
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
