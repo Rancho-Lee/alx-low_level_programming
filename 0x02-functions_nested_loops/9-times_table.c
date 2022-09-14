602b6abb7179ac6dbf1799eb81e1ed0774f95a21
Rancho-Lee <hairdrap7@gmail.com>
2022-09-14 15:32:05 -0700

    step9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,14 +10,14 @@ void times_table(void)
 	int x, y, z;
 
 	x = 0;
-	
+
 	while (x < 10)
 	{
 		z = 0;
 		while (z < 10)
 		{
 			y = x * z;
-			_putchar((y % 10) + '0');
+			_putchar(y + '0');
 			if (z != 9)
 			{
 				_putchar(',');
