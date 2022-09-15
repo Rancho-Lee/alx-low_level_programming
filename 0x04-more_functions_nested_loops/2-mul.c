2650885247b9bc89302aa2ec81a65d5162a0e3b8
Rancho-Lee <hairdrap7@gmail.com>
2022-09-15 15:18:16 -0700

    step2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * mul - multiply integers
+ *
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: int;
+ */
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = a * b;
+
+	return (mult);
+}
