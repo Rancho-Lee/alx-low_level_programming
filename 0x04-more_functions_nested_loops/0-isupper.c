17f54712e93816cf0df218c706ae225c717c22d1
Rancho-Lee <hairdrap7@gmail.com>
2022-09-15 14:57:50 -0700

    step0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isupper - detremining uppercase
+ *
+ * @c: parameter to check for uppercase
+ *
+ * Return: int;
+ *
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a directory on more nested loops and functions, enjoy.
