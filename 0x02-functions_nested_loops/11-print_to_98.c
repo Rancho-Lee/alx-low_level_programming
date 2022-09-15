d9b06aeff8c25fc80e9629c199430ed3637670b7
Rancho-Lee <hairdrap7@gmail.com>
2022-09-14 17:17:03 -0700

    step10

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * print_to_98 - print value to 98
+ *
+ * @n: parameter to print from
+ *
+ */
+void print_t0_98(int n)
+{
+	if (n == 98)
+	{
+		printf("%d, ", n);
+	}
+	else if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else if (n >= 98)
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ")
+			}
+		}
+	}
+}
