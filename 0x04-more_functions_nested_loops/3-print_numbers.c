324dfbc9fef6b17c2ee1481f16b45936c09f3534
Rancho-Lee <hairdrap7@gmail.com>
2022-09-15 15:25:48 -0700

    step3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - printing of numbers
+ *
+ * Return:;
+ *
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
