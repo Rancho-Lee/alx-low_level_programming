7f2acca636e1eebaa51d30269c6a486b3950403f
Rancho-Lee <hairdrap7@gmail.com>
2022-09-15 19:30:53 -0700

    dgb2

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
+
 /**
- *
- * * main - causes an infinite loop
- *
- * * Return: 0
- *
- * */
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
 int main(void)
 {
 	int i;
@@ -20,6 +19,7 @@ int main(void)
 	 * putchar(i);
 	 * }
 	 */
+
         printf("Infinite loop avoided! \\o/\n");
 
        return (0);
