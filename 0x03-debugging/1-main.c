7ffb4c0813d4218daf26fd6ae5d6156a76b2ccfb
Rancho-Lee <hairdrap7@gmail.com>
2022-09-15 17:05:52 -0700

    step

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -14,11 +14,12 @@ int main(void)
 	
 	i = 0;
 	
-	while (i < 10)
-
-	{
-            putchar(i);/** This will always in an infinte loop*/
-        }
+	/**
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
         printf("Infinite loop avoided! \\o/\n");
 
        return (0);
