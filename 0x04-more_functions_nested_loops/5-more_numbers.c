531ebf6448f6da891aaa389b10e96f7d198e6fcb
Rancho-Lee <hairdrap7@gmail.com>
2022-09-15 19:13:47 -0700

    step8

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,19 +7,18 @@
  */
 void more_numbers(void)
 {
-	int num, space;
+	int space, num;
 
 	for (space = 0; space < 10; space++)
 	{
 		for (num = 0; num < 15; num++)
 		{
-			if (num > 9)
+			if (num >= 10)
 			{
-				_putchar((num / 10) + '0');
+				_putchar(num / 10 + '0');
 			}
-			_putchar((num % 10) + '0');
+			_putchar(num % 10 + '0');
 		}
 		_putchar('\n');
 	}
-	return ;
 }
