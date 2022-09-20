b4cc507ce59772c84a2b1b90cb0f381982fcd4a9
Rancho-Lee <hairdrap7@gmail.com>
2022-09-20 13:08:41 -0700

    step3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - printing an array of char
+ * @str: string to be printed
+ * Return: 0;
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
