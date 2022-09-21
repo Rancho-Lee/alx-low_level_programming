8a9c13cb924b89025ada626f1a77df2028f3edb1
Rancho-Lee <hairdrap7@gmail.com>
2022-09-21 02:25:31 -0700

    step7

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,13 +5,17 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int i, j;
 
 	i = 0;
 
 	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
 		i++;
 	}
+	for (j = 0; j <= i; j += 2)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
 }
