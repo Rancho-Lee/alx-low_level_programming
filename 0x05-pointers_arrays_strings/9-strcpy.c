bc420839e9993541affcaa8cf51b937180a4f9d9
Rancho-Lee <hairdrap7@gmail.com>
2022-09-21 11:08:30 -0700

    step5

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _strcpy - copying one string to the other
+ * *_strcpy - copying one string to the other
  * @src: string to copy from
  * @dest: string to copy to
  * Return: character dest
  */
-char _strcpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int i;
 
