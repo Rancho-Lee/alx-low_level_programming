89109d7bb93953b6e230ae35a805e87219d7d4a2
Rancho-Lee <hairdrap7@gmail.com>
2022-09-21 01:49:10 -0700

    step4

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - reverse a string
+ * @s: string to be reversed
+ * Return: 0;
+ */
+void rev_string(char *s)
+{
+	int i, j, k;
+
+	i = 0;
+
+	while (i != '\0')
+	{
+		i++;
+	}
+
+	j = i;
+
+	for (k = j - 1; k >= 0; k--)
+		return (k);
+}
