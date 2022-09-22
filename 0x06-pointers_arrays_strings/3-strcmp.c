8bc8a494abc3c6bd53b0d9cdde8e146328bf2258
Rancho-Lee <hairdrap7@gmail.com>
2022-09-22 10:48:02 -0700

    step3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,6 +8,8 @@ int _strcmp(char *s1, char *s2)
 {
 	int i, result;
 
+	i = 0;
+
 	while (s1[i] == s2[i])
 	{
 		if (s1[i] == '\0' && s2[i] == '\0')
@@ -15,5 +17,6 @@ int _strcmp(char *s1, char *s2)
 		i++
 	}
 	result = s1[i] - s2[i];
+	
 	return (result);
 }
