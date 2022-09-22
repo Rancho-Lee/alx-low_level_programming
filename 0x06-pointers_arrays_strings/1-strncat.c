32e21796e1701002ca6442470d9c721318a8508b
Rancho-Lee <hairdrap7@gmail.com>
2022-09-22 07:21:18 -0700

    step1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncat - catenating specific number of strings
+ * @dest: the destination of new string
+ * @src: source of the string
+ * @n: number of string to be catenated
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int len, i;
+
+	len = 0;
+
+	while (dest[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < n && src[i] != '\0', i++, len++)
+	{
+		dest[len] = src[i];
+	}
+	dest[len] = '\0';
+
+	return (dest);
+}
