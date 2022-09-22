de8fc32692b649da66634bcfc0230917c3d6e5d5
Rancho-Lee <hairdrap7@gmail.com>
2022-09-22 10:46:07 -0700

    step3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**_strcmp - comparing strings
+ * @s1: first sttring
+ * @s2: second string
+ * Return: difference between the two strings;
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, result;
+
+	while (s1[i] == s2[i])
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		i++
+	}
+	result = s1[i] - s2[i];
+	return (result);
+}
