fb27bab6b12ce9c040c8746400326b807454ac97
Rancho-Lee <hairdrap7@gmail.com>
2022-09-22 06:17:20 -0700

    step0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcat - concatenating two strings
+ * @dest: destination of file
+ * @src: the file source
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+
+	while (dest[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; src[i] != '\0'; i++; len++)
+	{
+		dest[len] = src[i];
+	}
+	dest[len] = '\0';
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is a directory on more pointers and array for better understanding
