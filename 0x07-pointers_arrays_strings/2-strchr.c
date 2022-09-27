572abe43f085567ca874e3a3b65baa5f8646dafe
Rancho-Lee <hairdrap7@gmail.com>
2022-09-27 16:18:15 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - returning a pointer to the first string char
+ * @s: string to return from
+ * @c: character to check for in string
+ * Return: char;
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s + i); i++)
+	{
+		if (*(s + i) == c)
+		{
+			return (s + i);
+		}
+	}
+}
