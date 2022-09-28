27c1ada8156735959f8120cae7f9b1682b6b62e8
Rancho-Lee <hairdrap7@gmail.com>
2022-09-28 15:13:37 -0700

    recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - calculating the length of a string
+ * @s: string to calculate its lenght
+ * Return: int;
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	if (*s != '\0')
+	{
+		return 1 + _strlen_recursion(s + 1);
+	}
+}
---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#incude "main.h"
+/**
+ * factorial - calculating the factorial of an interger
+ * @n: interger to factorial
+ * Return: int;
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 1 || n == 0)
+		return (1);
+	return n * factorial(n - 1);
+}
