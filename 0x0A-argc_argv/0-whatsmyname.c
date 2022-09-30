0df0d6a0fa4e3645c30c9370a72b8d78e064c66e
Rancho-Lee <hairdrap7@gmail.com>
2022-09-30 11:13:28 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * whatsmyname - printing the program name
+ * @argc: count of argument
+ * @argv: array of argument variable
+ * Return: int;
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This directory is on argc (argument count) and argv (array of argument variable)
