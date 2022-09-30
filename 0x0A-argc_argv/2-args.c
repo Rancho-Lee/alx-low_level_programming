0123d5bee4634b745e916f48a01e823f53384e23
Rancho-Lee <hairdrap7@gmail.com>
2022-09-30 12:38:44 -0700

    printing_all

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - printing all arguments
  * @argc: argument count
@@ -12,6 +13,7 @@ int main(int argc, char *argv[])
 	 for (i = 0; i < argc; i++)
 	 {
 		 printf("%s\n", argv[i]);
+
 		 return (0);
 	 }
 }
