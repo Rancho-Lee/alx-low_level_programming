0309c29904062caf6b2693a6623c45868d9857e1
Rancho-Lee <hairdrap7@gmail.com>
2022-09-30 11:31:29 -0700

    numofar

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - printing the number of argument
+ * @argc: number of argument passed
+ * @argv: array of variable
+ * Return: int;
+ */
+int main(int argc, int argv[])
+{
+	(void)argv[];
+	printf("%d\n", argc);
+	return (0);
+}
