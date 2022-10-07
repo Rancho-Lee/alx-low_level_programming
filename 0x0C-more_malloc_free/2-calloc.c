6ca2ade1ec42194e64aabcc632058d69447913c6
Rancho-Lee <hairdrap7@gmail.com>
2022-10-07 15:19:46 -0700

    step2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _calloc - clean allocation
+ * @nmemb: number of member in array
+ * @size: size of array
+ * Return: VOID
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		*((char *)(p) + i) = 0;
+
+	return (p);
+}
