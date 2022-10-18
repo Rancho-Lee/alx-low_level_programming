8bf93e95d1ac6f235082dc8e3c7882834208f70d
Rancho-Lee <aoluwaseun77@gmail.com>
2022-10-18 13:10:38 -0700

    step0

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -24,6 +24,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (!nptr)
 		return (NULL);
 	optr = ptr;
+	
 	if (new_size < old_size)
 	{
 		for (i = 0; i < new_size; i++)
