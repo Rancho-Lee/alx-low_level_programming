dd98179a7cefc6aac8c6df87bbce21fa6353ee4c
Rancho-Lee <aoluwaseun77@gmail.com>
2022-10-20 16:10:46 -0700

    varfun0

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - printing of numbers
+ * @seperator: seperating numbers to be printed
+ * @n: number of int passed
+ * Return: 
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	size_t i;
+	int num_arg;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		num_arg = arg(ap, int);
+		if (!separator)
+			printf("%d", num_arg);
+		else if (separator && i == n - 1)
+			printf("%d", num_arg);
+		else
+			printf("%d%s", num_arg, separator);
+	}
+
+	va_end(ap);
+	printf('\n');
+}
