926598c9594afa3dbceeb0115d240c8ebf997bdb
Rancho-Lee <aoluwaseun77@gmail.com>
2022-10-20 15:30:05 -0700

    varfun0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,14 +7,14 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list ap;
+	int i, int sum;
+	
 	if (n == 0)
 		return (0);
-	va_list ap;
-
-	va_start(ap, n);
-
-	int i, int sum;
-
+	
+	va_start(ap);
+	
 	for (i = 0; i < n; i++)
 		sum += va_arg(ap, int);
 
