ddc71393693e745d74a7536277faabfc069db0f6
Rancho-Lee <aoluwaseun77@gmail.com>
2022-10-20 15:39:18 -0700

    varfun0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,4 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 /**
  * sum_them_all - sumation using variadic fun
  * @n: number of int to sum
@@ -8,7 +7,7 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
-	int i, int sum;
+	int i, int sum = 0;
 	
 	if (n == 0)
 		return (0);
