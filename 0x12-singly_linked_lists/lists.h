75c29fb91454122fc619873fafd6972956bbffbd
Rancho-Lee <aoluwaseun77@gmail.com>
2022-10-20 18:53:44 -0700

    singlestring

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,6 +4,15 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 
 size_t print_list(const list_t *h);
 int _putchar(char c);
