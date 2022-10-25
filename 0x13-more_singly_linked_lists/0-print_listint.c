b0278f06319535de8fba7c143ac84ae7ce858420
Rancho-Lee <aoluwaseun77@gmail.com>
2022-10-25 13:23:41 -0700

    singlink

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * size_t print_listint - print the content of list
+ * @h: linked list of type listint_t to print
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h);
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count++);
+}
