15e01ad247c7927b6a9664b096661c5e2e356d54
Rancho-Lee <aoluwaseun77@gmail.com>
2022-10-25 14:50:37 -0700

    singlylist

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * *add_nodeint_end - a dd node at the end 
+ * *add_nodeint_end - a dd node at the end
  * @head: pointer to the first node of the list
  * @n: data to be inserted in new node
  * Return: address of new element or null if failed
