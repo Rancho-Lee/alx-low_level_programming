f698f6cd4da5f752a5f96290d6a17b30564c7d36
Rancho-Lee <aoluwaseun77@gmail.com>
2022-10-25 14:30:40 -0700

    singlylist

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,7 +13,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (!new)
 		return (NULL);
 	new->n = n;
-	new->next = *head;
+	new->next = NULL;
 	*head = new;
 
 	return (new);
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,4 +13,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	if (!new)
 		return (NULL);
+
 }
