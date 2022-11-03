65772d342abb0ac5ac9abbf45458780ae1d7c89f
Rancho-Lee <aoluwaseun77@gmail.com>
2022-11-03 16:04:40 -0700

    step0

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,21 +1,29 @@
 #ifndef MAIN_H
+
 #define MAIN_H
 
-int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
-ssize_t read_textfile(const char *filename, size_t letters);
-int _putchar(char c);
-#include <stdlib.h>                                                                                                                                             
 
-#include <stdio.h>                                                                                                                                              
 
-#include <sys/types.h>                                                                                                                                          
+#include <stdlib.h>
 
-#include <sys/stat.h>                                                                                                                                           
-
-#include <fcntl.h> 
+#include <stdio.h>
 
 #include <unistd.h>
+
+#include <fcntl.h>
+
+#include <sys/types.h>
+
+#include <sys/stat.h>
+
+
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+int create_file(const char *filename, char *text_content);
+
+int append_text_to_file(const char *filename, char *test_content);
+
 
 
 #endif
