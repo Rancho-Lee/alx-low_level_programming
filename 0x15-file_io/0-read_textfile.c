8c7769824598bcc73b20fb16eaaead8be68926bf
Rancho-Lee <aoluwaseun77@gmail.com>
2022-11-03 15:55:40 -0700

    step0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (fd < 0)
 		return (0);
 
 	buf = malloc(sizeof(char) * letters);
