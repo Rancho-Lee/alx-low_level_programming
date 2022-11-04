0de55f8e277c450a23bf47c9b10a9e5451178769
Rancho-Lee <aoluwaseun77@gmail.com>
2022-11-03 18:01:33 -0700

    step2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,12 +15,16 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
 
 	if (text_content)
 	{
 		while (text_content[s])
 			s++;
 		t = write(fd, text_content, s);
+		if (t != s)
+			return (-1);
 	}
 
 	close(fd);
