7ce77e9fcbe6e0a4e580074a4c3f99215611ff77
Rancho-Lee <aoluwaseun77@gmail.com>
2022-11-03 17:56:44 -0700

    step2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * create_file - create a file
- * @filename : name of file 
+ * @filename : name of file
  * @text_content: content in text
  * Return: int
  */
@@ -15,14 +15,14 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	
+
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
 
 	if (text_content)
 	{
-		while(text_content[s])
+		while (text_content[s])
 			s++;
 		t = write(fd, text_content, s);
 		if (t != s)
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * append_text_to_file - append a text to end of fiel
+ * @filename : name of the file
+ * @text_content : content of the text
+ * Return: (0);
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, s, t;
+
+	s = 0;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (text_content)
+	{
+		while (text_content[s])
+			s++;
+		t = write(fd, text_content, s);
+	}
+
+	close(fd);
+	return (1);
+}
