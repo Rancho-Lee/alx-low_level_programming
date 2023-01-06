d0862963318c35b45124dc6d7a23d4cf961618e1
Rancho-Lee <hairdrap7@gmail.com>
2023-01-06 06:00:18 -0800

    hsh

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,8 +15,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_ht->array = malloc(sizeof(void *) * size);
 	if (!new_ht->array)
 		return (NULL);
+
 	new_ht->size = size;
+
 	for (i = 0; i < size; i++)
 		new_ht->array[i] = NULL;
+
 	return (new_ht);
 }
